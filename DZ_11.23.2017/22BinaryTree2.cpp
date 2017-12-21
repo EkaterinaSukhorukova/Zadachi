a158984e77962c3e3782d1eb65cd19227a6c82e7
EkaterinaSukhorukova <32143980+EkaterinaSukhorukova@users.noreply.github.com>
2017-12-21 04:32:41 +0300

    Create 22BinaryTree2.cpp

---
+++ DZ_11.23.2017/22BinaryTree2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std; 
+
+struct node
+{
+    int info;
+    node *l, *r;
+};
+ 
+node * tree=NULL;
+ 
+
+void push(int a,node **t)
+{
+    if ((*t)==NULL)
+    {
+        (*t)=new node;
+        (*t)->info=a;
+        (*t)->l=(*t)->r=NULL;
+        return;
+    }
+
+        if (a>(*t)->info) push(a,&(*t)->r);
+        else push(a,&(*t)->l);
+}
+ 
+
+void print (node *t,int u) 
+{
+    if (t==NULL) return;
+    else
+    {
+    print(t->l,++u);
+    for (int i=0;i<u;++i) cout<<"|";
+    cout<<t->info<<endl;
+    u--;
+    }
+    print(t->r,++u);
+}
+ 
+int main ()
+{   
+    int n;
+    int s;
+    cin>>n;
+ 
+    for (int i=0;i<n;++i)
+    {
+    cin>>s;
+    push(s,&tree);
+    }
+    print(tree,0);
+  
+    return 0;
+}
