66f9136dcd6ae033d77a7a9f4bcae8b70cd0e7de
EkaterinaSukhorukova <32143980+EkaterinaSukhorukova@users.noreply.github.com>
2018-03-01 00:12:51 +0300

    Create heapSort.cpp

---
+++ DZ_03.01.2018/heapSort.cpp
@@ -0,0 +1,16 @@
+#include "func.h"
+using namespace std;
+
+void heapSort(int arr[], int n)
+{
+	for (int i = n / 2 - 1; i >= 0; i--)
+    {
+    	heapify(arr, n, i);
+    }
+    
+    for (int i=n-1; i>=0; i--)
+    {
+    	swap(arr[0], arr[i]);
+    	heapify(arr, i, 0);
+    }
+}
