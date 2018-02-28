180c9f10669b4f10931f119130d26dd28fc2e219
EkaterinaSukhorukova <32143980+EkaterinaSukhorukova@users.noreply.github.com>
2018-03-01 00:20:22 +0300

    Create main.cpp

---
+++ DZ_03.01.2018/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "func.h"
+using namespace std;
+
+int main() 
+{
+	int n; 
+	cin >> n;
+	int *arr = new int[n]; 
+	for (int i=0; i < n; i++) 
+	{  
+		cin >> arr[i]; 
+	}
+	
+	heapSort(arr, n);
+	printArray(arr, n);
+
+	return 0;
+}
