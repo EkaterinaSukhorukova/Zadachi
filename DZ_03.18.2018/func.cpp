63bf4746689774aaae09bf69482b0611df4de77d
EkaterinaSukhorukova <32143980+EkaterinaSukhorukova@users.noreply.github.com>
2018-03-18 23:48:34 +0300

    Create func.cpp

---
+++ DZ_03.18.2018/func.cpp
@@ -0,0 +1,17 @@
+template <typename T>
+quat<T> operator*(quat<T> &a, quat<T> &b)
+{
+    return quat<T>( a.X*b.X - a.Y*b.Y - a.Z*b.Z - a.W*b.W,
+                    a.X*b.Y + b.X*a.Y + a.Z*b.W - b.Z*a.W,
+                    a.X*b.Z + b.X*a.Z + a.W*b.Y - a.Y*b.W,
+                    a.X*b.W + b.X*a.W + a.Y*b.Z - a.Z*b.Y);
+};
+
+template <typename T>
+quat<T> operator+(quat<T> &a, quat<T> &b)
+{
+    return quat<T>( a.X + b.X,
+                    a.Y + b.Y,
+                    a.Z + b.Z,
+                    a.W + b.W);
+};
