b078d2a18580e7f0c9f8096dd7694c6670525d4a
EkaterinaSukhorukova <32143980+EkaterinaSukhorukova@users.noreply.github.com>
2018-05-22 22:57:13 +0300

    Update Kod.cpp

--- проекта/Kod.cpp
+++ проекта/Kod.cpp
@@ -2,21 +2,34 @@
 #include <cstdlib>
 #include <vector>
 #include <cmath>
+#include <stack>
+#include <ctime>
+#include <algorithm>
+#include <set>
 
 using namespace std;
 
+const double EPS(1e-5);
+
 struct Point {
-    float x;
-    float y;
+    double x, y;
+    Point() {}
+    Point(double x, double y) : x(x), y(y) {}
+} p0;
+
+bool operator <(const Point &a, const Point &b) {
+   	return a.y < b.y || (abs(a.y - b.y) < EPS && a.x < b.x);
 }
 
 struct Direct {
-	float a;
-	float b;
-	float c;
+	double a, b, c;
+};
+
+double dist(const Point &p1, const Point &p2) {
+	return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
 }
 
-Point nextToTop(stack<Point> &S){
+Point nextToTop(stack <Point> &S) {
     Point p = S.top();
     S.pop();
     Point res = S.top();
@@ -24,261 +37,203 @@ Point nextToTop(stack<Point> &S){
     return res;
 }
 
-int swap(Point &p1, Point &p2){
-    Point temp = p1;
-    p1 = p2;
-    p2 = temp;
-    return 1; //Успех
+
+
+//Проверка на левую тройку векторов
+bool orientation(const Point &p0, const Point &p1, const Point &p2) {
+    return (p0.x * p1.y + p1.x * p2.y + p2.x * p0.y - p0.y * p1.x - p1.y * p2.x - p2.y * p0.x) / 2 > EPS;
 }
 
-int orientation(Point p, Point q, Point r){
-    int val = (q.y - p.y) * (r.x - q.x) -
-              (q.x - p.x) * (r.y - q.y);
+bool compare(const Point &p1, const Point &p2) {
+	if (dist(p0, p1) < EPS)
+		return true;
+	if (dist(p0, p2) < EPS)
+		return false;
 
-    if (val == 0) return 0;  // коллинеарны
-    return (val > 0)? 1: 2; // по часовой стрелке или против
-}
+	if (orientation(p0, p1, p2))
+		return true;
+	if (orientation(p0, p2, p1))
+		return false;
 
-int compare(const void *vp1, const void *vp2){
-    Point *p1 = (Point *)vp1;
-    Point *p2 = (Point *)vp2;
-
-    // Находим ориентацию
-    int o = orientation(p0, *p1, *p2);
-    if (o == 0)
-        return (dist(p0, *p2) >= dist(p0, *p1))? -1 : 1;
-
-    return (o == 2)? -1: 1;
-}
-
-void GrahamScan(Point points[], int n, std::vector<Point> &result){
-    // Находим самую нижнюю точку
-    float ymin = points[0].y, min = 0;
-    for (int i = 1; i < n; i++){
-        int y = points[i].y;
-
-        // Выбираем самую нижнюю или самую левую из них в случае совпадения
-        if ((y < ymin) || (ymin == y && points[i].x < points[min].x))
-            ymin = points[i].y, min = i;
-    }
-
-    // Помещаем самую левую точку в начало
-    swap(points[0], points[min]);
-
-    // Сортируем n-1 точек по отношению к первой точке.  Точка p1 становится
-    // перед p2 в отсортированной последовательности, если p2 имеет больший полярный угол (в 
-    // направлении по часовой стрелке), чем p1
-    p0 = points[0];
-    qsort(&points[1], n-1, sizeof(Point), compare);
-
-    // Создаем пустой стек и кладем в него первые три точки.
-    stack<Point> S;
-    S.push(points[0]);
-    S.push(points[1]);
-    S.push(points[2]);
-
-    // Обрабатываем оставшиеся n-3 точек
-    for (int i = 3; i < n; i++){
-        // Продолжаем удалять верхнюю точку, пока угол, сформированный точками next-to-top, 
-        // top, and points[i] не образует левый поворот
-        if  (S.size() >= 2){
-            while  (orientation(nextToTop(S), S.top(), points[i]) != 2)
-                S.pop();
-            S.push(points[i]);
-        }
-    }
-
-     // Теперь в стеке нужные точки
-    while (!S.empty()){
-        Point p = S.top();
-        //cout << "(" << p.x << ", " << p.y <<")" << endl;
-        result.push_back(p);
-        S.pop();
-    }
+	return dist(p0, p1) > dist(p0, p2);
 }
 
 
-//Алгоритм генерации выпуклого многоугольника
 
-void genLargeRectangle(int amount, std::vector<g::Point> &rect, int &generated){
-    //Генерируем множество из amount точек
-    g::Point *p = new g::Point[amount];
-    genRandPointVariety(p, amount);
-    //Строим для них выпуклую оболочку по Грэхему
-    std::vector<g::Point> convex_hull;
-    GrahamScan(p, amount, convex_hull);
-    //Количество точек в оболочке (вершин многоугольника)
-    generated = convex_hull.size();
-    //Копируем точки из вектора в массив rect
-    rect.assign(convex_hull.begin(), convex_hull.end());
-    delete[] p;
+void GrahamScan(vector <Point> &points) {
+	// Находим самую нижнюю точку
+	size_t min = 0;
+	for (size_t i = 1; i < points.size(); i++)
+	    // Выбираем самую нижнюю или самую левую из них в случае совпадения
+	    if (points[i] < points[min])
+	        min = i;
+
+	p0 = points[min];
+
+	// Сортируем точки по отношению к нижней. Точка p1 становится
+	// перед p2 в отсортированной последовательности, если p2 имеет больший полярный угол
+	// (в направлении против часовой стрелке), чем p1
+	sort(points.begin(), points.end(), &compare);
+
+	// Создаем пустой стек и кладем в него первые три точки.
+	stack <Point> S;
+	S.push(points[0]);
+	S.push(points[1]);
+	S.push(points[2]);
+
+	// Обрабатываем оставшиеся точки
+	for (size_t i = 3; i < points.size(); i++) {
+		// Продолжаем удалять верхнюю точку, пока угол, сформированный точками next-to-top, 
+		// top, and points[i] не образует левый поворот
+		while (S.size() > 1 && !orientation(nextToTop(S), S.top(), points[i]))
+			S.pop();
+		S.push(points[i]);
+	}
+	// Теперь в стеке нужные точки
+	points.resize(S.size());
+	for (size_t i = 0; !S.empty(); i++, S.pop())
+		points[i] = S.top();
 }
-    
- 
+	
 //Функция генерации множества точек
-
-void genRandPointVariety(g::Point points[], int amount){
-    std::vector<g::Point> v;
-    g::Point *p = NULL;
-
+void genRandPointVariety(const size_t amount, vector <Point> &points) {
+    set <Point> s;
     //Генерируем множество произвольных точек в количестве amount
     srand(time(NULL));
-    for(int i = 0; i < amount; i++){
-        float x = std::rand() % 512;
-        float y = std::rand() % 512;
-        p = new g::Point();
-        p->x = x;
-        p->y = y;
-        v.push_back(*p);
-        delete p;
-    }
+    while (s.size() < amount)
+    	s.insert(Point(rand() % 512, rand() % 512));
 
-    //Удаляем повторяющиеся точки, если такие есть
-    std::vector<g::Point>::iterator it;
-    it = std::unique(v.begin(), v.end(), pt_cmp);
-    v.erase(it, v.end());
-
-    //Если после удаления точек в векторе меньше, чем нужно,
-    //Добавляем по одной, проверяя уникальность
-    while(v.size() < amount){
-        float x = std::rand() % 335;
-        float y = std::rand() % 335;
-        p = new g::Point();
-        p->x = x;
-        p->y = y;
-        v.push_back(*p);
-        it = std::unique(v.begin(), v.end(), pt_cmp);
-        v.erase(it, v.end());
-        delete p;
-    }
-
-    for(int i = 0; i < amount; i++)
-        points[i] = v[i];
+	size_t i = 0;
+    points.resize(amount);
+    for(auto it: s)
+        points[i++] = it;
 }
-   
-   
+
+//Алгоритм генерации выпуклого многоугольника
+size_t genLargeRectangle(const size_t amount, vector <Point> &rect) {
+    //Генерируем множество из amount точек
+    genRandPointVariety(amount, rect);
+    //Строим для них выпуклую оболочку по Грэхему
+    GrahamScan(rect);
+    return rect.size();
+}
+
+
+
 //Функция заполняющая массив структур, прилегащих к точке многоугольника (вершине)
 //И уравнение биссектриссы угла (задается координатами направляющего вектора )
 
-void PointDirection (Point p[], Direct s[][], Point bisec[]){
-	int n = sizeof (p[]);
-	for (int i=0, i<n, i++)
-	{
-		if i=0
-		{	
-			s[0][0].a = p[n].y-p[0].y;
-			s[0][0].b = p[0].x-p[n].x;
-			s[0][0].c = p[0].y*p[n].x-p[0].x*p[n].y;
-			
-			s[1][0].a = p[1].y-p[0].y;
-			s[1][0].b = p[0].x-p[1].x;
-			s[1][0].c = p[0].y*p[1].x-p[0].x*p[1].y;
-			
-			bisec[0].x = ((p[n].x-p[0].x)/sqrt((p[n].x-p[0].x)*(p[n].x-p[0].x)+(p[n].y-p[0].y)*(p[n].y-p[0].y)) + ((p[1].x-p[0].x)/sqrt((p[1].x-p[0].x)*(p[1].x-p[0].x)+(p[1].y-p[0].y)*(p[1].y-p[0].y));
-			bisec[0].y = ((p[n].y-p[0].y)/sqrt((p[n].x-p[0].x)*(p[n].x-p[0].x)+(p[n].y-p[0].y)*(p[n].y-p[0].y)) + ((p[1].y-p[0].y)/sqrt((p[1].x-p[0].x)*(p[1].x-p[0].x)+(p[1].y-p[0].y)*(p[1].y-p[0].y));	
+void PointDirection(vector <Point> &p, vector <Direct> s[], vector <Point> &bisec) {
+	size_t n = p.size() - 1;
+	for (size_t i = 0; i <= n; i++) {
+		if (i == 0) {
+			s[0][0].a = p[n-1].y - p[0].y;
+			s[0][0].b = p[0].x - p[n-1].x;
+			s[0][0].c = p[0].y * p[n-1].x - p[0].x * p[n-1].y;
+
+			s[1][0].a = p[1].y - p[0].y;
+			s[1][0].b = p[0].x - p[1].x;
+			s[1][0].c = p[0].y * p[1].x - p[0].x * p[1].y;
+
+			bisec[0].x = (p[n-1].x - p[0].x) / sqrt(dist(p[0], p[n-1])) + (p[1].x - p[0].x) / sqrt(dist(p[0], p[1]));
+			bisec[0].y = (p[n-1].y - p[0].y) / sqrt(dist(p[0], p[n-1])) + (p[1].y - p[0].y) / sqrt(dist(p[0], p[1]));	
 		}
 		
-		if (i=n)
-		{	
-			s[0][n].a = p[n-1].y-p[n].y;
-			s[0][n].b = p[n].x-p[n-1].x;
-			s[0][n].c = p[n].y*p[n-1].x-p[n].x*p[n-1].y;
-			
-			s[1][n].a = p[0].y-p[n].y;
-			s[1][n].b = p[n].x-p[0].x;
-			s[1][n].c = p[n].y*p[0].x-p[n].x*p[0].y;
-			
-			bisec[n].x = ((p[n-1].x-p[n].x)/sqrt((p[n-1].x-p[n].x)*(p[n-1].x-p[n].x)+(p[n-1].y-p[n].y)*(p[n-1].y-p[n].y)) + ((p[n+1].x-p[n].x)/sqrt((p[n+1].x-p[n].x)*(p[n+1].x-p[n].x)+(p[n+1].y-p[n].y)*(p[n+1].y-p[n].y));
-			bisec[n].y = ((p[n-1].y-p[n].y)/sqrt((p[n-1].x-p[n].x)*(p[n-1].x-p[n].x)+(p[n-1].y-p[n].y)*(p[n-1].y-p[n].y)) + ((p[n+1].y-p[n].y)/sqrt((p[n+1].x-p[n].x)*(p[n+1].x-p[n].x)+(p[n+1].y-p[n].y)*(p[n+1].y-p[n].y));
-			
+		if (i == n-1)	{	
+			s[0][n-1].a = p[n - 2].y - p[n-1].y;
+			s[0][n-1].b = p[n-1].x - p[n - 2].x;
+			s[0][n-1].c = p[n-1].y * p[n - 2].x - p[n-1].x * p[n - 2].y;
+
+			s[1][n-1].a = p[0].y - p[n-1].y;
+			s[1][n-1].b = p[n-1].x - p[0].x;
+			s[1][n-1].c = p[n-1].y * p[0].x - p[n-1].x * p[0].y;
+
+			bisec[n-1].x = (p[n - 2].x - p[n-1].x) / sqrt(dist(p[n - 2], p[n-1])) + (p[0].x - p[n-1].x) / sqrt(dist(p[n-1], p[0]));
+			bisec[n-1].y = (p[n - 2].y - p[n-1].y) / sqrt(dist(p[n - 2], p[n-1])) + (p[0].y - p[n-1].y) / sqrt(dist(p[n-1], p[0]));
 		}
-		
-		if ((i != n) and (i != 0)){
-			s[0][i].a = p[i-1].y-p[i].y;
-			s[0][n].b = p[i].x-p[i-1].x;
-			s[0][n].c = p[i].y*p[i-1].x-p[i].x*p[i-1].y;
-			
-			s[1][n].a = p[i+1].y-p[i].y;		
-			s[1][n].b = p[i].x-p[i+1].x;
-			s[1][n].c = p[i+1].y*p[i].x-p[I+1].x*p[i].y;
-			
-			bisec[i].x = ((p[i-1].x-p[i].x)/sqrt((p[i-1].x-p[i].x)*(p[i-1].x-p[i].x)+(p[i-1].y-p[i].y)*(p[i-1].y-p[i].y)) + ((p[i+1].x-p[i].x)/sqrt((p[i+1].x-p[i].x)*(p[i+1].x-p[i].x)+(p[i+1].y-p[i].y)*(p[i+1].y-p[i].y));
-			bisec[i].y = ((p[i-1].y-p[i].y)/sqrt((p[i-1].x-p[i].x)*(p[i-1].x-p[i].x)+(p[i-1].y-p[i].y)*(p[i-1].y-p[i].y)) + ((p[i+1].y-p[i].y)/sqrt((p[i+1].x-p[i].x)*(p[i+1].x-p[i].x)+(p[i+1].y-p[i].y)*(p[i+1].y-p[i].y));
+
+		if (i != n && i != 0) {
+			s[0][i].a = p[i - 1].y - p[i].y;
+			s[0][i].b = p[i].x - p[i - 1].x;
+			s[0][i].c = p[i].y * p[i - 1].x - p[i].x * p[i - 1].y;
+
+			s[1][i].a = p[i + 1].y - p[i].y;
+			s[1][i].b = p[i].x - p[i + 1].x;
+			s[1][i].c = p[i + 1].y * p[i].x - p[i + 1].x * p[i].y;
+
+			bisec[i].x = (p[i - 1].x - p[i].x) / sqrt(dist(p[i - 1], p[i])) + (p[i + 1].x - p[i].x) / sqrt(dist(p[i], p[i + 1]));
+			bisec[i].y = (p[i - 1].y - p[i].y) / sqrt(dist(p[i - 1], p[i])) + (p[i + 1].y - p[i].y) / sqrt(dist(p[i], p[i + 1]));
 		}
-		
 	}
-	
 }
+
+
 
 //Функция нахождения узлов скелета 
 
-void Skelet (Point p[], Direct s[][], Point bisec[], Point uzel[]){
-	int n = sizeof (p[]);
-	float t;
-	float r;
-	int k=0;
+void Skelet(vector <Point> &p, vector <Direct> s[], vector <Point> bisec, vector <Point> uzel) {
+	double t, r, x0, y0;
+	size_t k = 0, n = p.size();
 	int schet = 0;
-	
-	while (k != n){
-		for (int i=0, i<n, i++){
-			if (i != k){
-				t=((s[2][i].c+s[2][i].b*p[k].y+s[2][i].a*p[k].x)/sqrt(s[2][i].a*s[2][i].a+s[2][i].b*s[2][i].b) - (s[2][k].c+s[2][k].b*p[k].y+s[2][k].a*p[k].x)/sqrt(s[2][k].a*s[2][k].a+s[2][k].b*s[2][k].b))/((bisec[k].x*s[2][k].a-bisec[k].y*s[2][k].b)/sqrt(s[2][i].a*s[2][i].a+s[2][i].b*s[2][i].b)+(bisec[k].y*s[2][k].b-bisec[k].x*s[2][k].a)/sqrt(s[2][k].a*s[2][k].a+s[2][k].b*s[2][k].b));
-				float x0 = bisec[k].x*t+p[k].x;
-				float y0 = bisec[k].y*t+p[k].y;
+	while (k != n-1) {
+		for (size_t i = 0; i < n; i++) {
+			if (i != k) {
+				t = ((s[0][i].c + s[0][i].b * p[k].y + s[1][i].a * p[k].x) / sqrt(s[1][i].a * s[1][i].a + s[1][i].b * s[1][i].b)
+					-(s[1][k].c + s[1][k].b * p[k].y + s[1][k].a * p[k].x) / sqrt(s[1][k].a * s[1][k].a + s[1][k].b * s[1][k].b))
+					/((bisec[k].x * s[1][k].a - bisec[k].y * s[1][k].b) / sqrt(s[1][i].a * s[1][i].a + s[1][i].b * s[1][i].b)
+					+ (bisec[k].y * s[1][k].b - bisec[k].x * s[1][k].a) / sqrt(s[1][k].a * s[1][k].a + s[1][k].b * s[1][k].b));
+				x0 = bisec[k].x * t + p[k].x;
+				y0 = bisec[k].y * t + p[k].y;
 
-				if (((s[1][k].a*x0+s[1][k].b*y0+s[1][k].c)<0)and((s[1][k].a*p[k+1].x+s[1][k].b*p[K+1].y+s[1][k].c)<0)) or (((s[1][k].a*x0+s[1][k].b*y0+s[1][k].c)>0)and((s[1][k].a*p[k+1].x+s[1][k].b*p[K+1].y+s[1][k].c)>0)){
-					r = abs(s[2][k].a*x0+s[2][k].b*y0+s[2][k].c)/sqrt(s[2][k].a*s[2][k].a+s[2][k].b*s[2][k].b);
-					for (int j=0, j<n, j++){
-						if ((j != k) and (j != i)){
-							if ((abs(s[2][j].a*x0+s[2][j].b*y0+s[2][j].c)/sqrt(s[2][j].a*s[2][j].a+s[2][j].b*s[2][j].b))<r){
-								schet = schet+1;
-							}
-						}
-					}
-				}
-				else{
-					t=((s[2][i].c-s[2][i].b*p[k].y-s[2][i].a*p[k].x)/sqrt(s[2][i].a*s[2][i].a+s[2][i].b*s[2][i].b) - (s[2][k].c+s[2][k].b*p[k].y+s[2][k].a*p[k].x)/sqrt(s[2][k].a*s[2][k].a+s[2][k].b*s[2][k].b))/((bisec[k].x*s[2][k].a+bisec[k].y*s[2][i].b)/sqrt(s[2][i].a*s[2][i].a+s[2][i].b*s[2][i].b)+(bisec[k].y*s[2][k].b+bisec[k].x*s[2][i].a)/sqrt(s[2][k].a*s[2][k].a+s[2][k].b*s[2][k].b));
-					r = abs(s[2][k].a*x0+s[2][k].b*y0+s[2][k].c)/sqrt(s[2][k].a*s[2][k].a+s[2][k].b*s[2][k].b);
-					for (int j=0, j<n, j++){
-						if ((j != k) and (j != i)){
-							if ((abs(s[2][j].a*x0+s[2][j].b*y0+s[2][j].c)/sqrt(s[2][j].a*s[2][j].a+s[2][j].b*s[2][j].b))<r){
-								schet = schet+1;
-							}
-						}
-					}
+				if ((s[0][k].a * x0 + s[0][k].b * y0 + s[0][k].c < 0 && s[0][k].a * p[k + 1].x + s[0][k].b * p[k + 1].y + s[0][k].c < 0)
+				 || (s[0][k].a * x0 + s[0][k].b * y0 + s[0][k].c > 0 && s[0][k].a * p[k + 1].x + s[0][k].b * p[k + 1].y + s[0][k].c > 0)) {
+					r = abs(s[1][k].a * x0 + s[1][k].b * y0 + s[1][k].c) / sqrt(s[1][k].a * s[1][k].a + s[1][k].b * s[1][k].b);
+					for (size_t j = 0; j < n; j++)
+						if (j != k && j != i)
+							if (abs(s[1][j].a * x0 + s[1][j].b * y0 + s[1][j].c) / sqrt(s[1][j].a * s[1][j].a + s[1][j].b * s[1][j].b) < r)
+								schet++;
+				} else {
+					t = ((s[1][i].c - s[1][i].b * p[k].y - s[1][i].a * p[k].x) / sqrt(s[1][i].a * s[1][i].a + s[1][i].b * s[1][i].b)
+						-(s[1][k].c + s[1][k].b * p[k].y + s[1][k].a * p[k].x) / sqrt(s[1][k].a * s[1][k].a + s[1][k].b * s[1][k].b))
+						/((bisec[k].x * s[1][k].a + bisec[k].y * s[1][i].b) / sqrt(s[1][i].a * s[1][i].a + s[1][i].b * s[1][i].b)
+						+ (bisec[k].y * s[1][k].b + bisec[k].x * s[2][i].a) / sqrt(s[1][k].a * s[1][k].a + s[1][k].b * s[1][k].b));
+					r = abs(s[1][k].a * x0 + s[1][k].b * y0 + s[1][k].c) / sqrt(s[1][k].a*s[1][k].a + s[1][k].b * s[1][k].b);
+					for (size_t j = 0; j < n; j++)
+						if (j != k && j != i)
+							if (abs(s[1][j].a * x0 + s[1][j].b * y0 + s[1][j].c) / sqrt(s[1][j].a * s[1][j].a + s[1][j].b * s[1][j].b) < r)
+								schet++;
 				}
 			}
-			
-			if (schet == 0){
-				uzel[k].x=x0;
-				uzel[k].y=y0;
-				k = k + 1;
+
+			if (schet == 0) {
+				uzel[k].x = x0;
+				uzel[k].y = y0;
+				k++;
 			}
 		}
 	}
 }
 
-//Рисовалка :)
-
-void Drow (Point p[], Point uzel[]){
-	for (int i=0, i<n, i++){
-		if (i=n-1){
-			Line (p[n].x, p[n].y, p[0].x, p[0].y);
-			Line (p[n].x, p[n].y, uzel[n].x, uzel[n].y);
-			Line (uzel[n].x, uzel[n].y, uzel[0].x, uzel[0].y);
-		}
-		else{
-			Line (p[i].x, p[i].y, p[i+1].x, p[i+1].y);
-			Line (p[i].x, p[i].y, uzel[i].x, uzel[i].y);
-			Line (uzel[i].x, uzel[i].y, uzel[i+1].x, uzel[i+1].y);
-		}
-	}
-}
 
 
-int main{
-   
-   
-	
+
+
+int main() {
+	size_t n;
+	cin >> n;
+	vector <Point> rect;
+	n = genLargeRectangle(n, rect);
+	vector <Direct> s[2];
+	s[0].resize(n), s[1].resize(n);
+	vector <Point> bisec, uzel;
+	bisec.resize(n), uzel.resize(n);
+	PointDirection(rect, s, bisec);
+	Skelet(rect, s, bisec, uzel);
+	cout << "Rectangle:" << endl;
+	for (size_t i = 0; i < n; ++i)
+		cout << '(' << rect[i].x << ", " << rect[i].y << ')' << endl;
+	cout << endl << "Skelet:" << endl;
+	for (size_t i = 0; i < n; ++i)
+		cout << '(' << uzel[i].x << ", " << uzel[i].y << ')' << endl;
 	return 0;
 }
