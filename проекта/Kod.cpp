76a9342e241d4b57674f7cf3a6bf89e961d97fc7
EkaterinaSukhorukova <32143980+EkaterinaSukhorukova@users.noreply.github.com>
2018-05-24 16:08:40 +0300

    Update Kod.cpp

--- проекта/Kod.cpp
+++ проекта/Kod.cpp
@@ -26,7 +26,7 @@ struct Direct {
 };
 
 double dist(const Point &p1, const Point &p2) {
-	return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+	return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
 }
 
 Point nextToTop(stack <Point> &S) {
