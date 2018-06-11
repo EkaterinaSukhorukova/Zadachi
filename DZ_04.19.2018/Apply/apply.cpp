e39cf0e8e26aecea43c67186fbc668d029ab96dd
EkaterinaSukhorukova <32143980+EkaterinaSukhorukova@users.noreply.github.com>
2018-06-11 15:41:27 +0300

    Update apply.cpp

--- DZ_04.19.2018/Apply/apply.cpp
+++ DZ_04.19.2018/Apply/apply.cpp
@@ -1,75 +1,23 @@
-// applyfunc.cpp: определяет точку входа для консольного приложения.
-//
-
-#include "stdafx.h"
-#include <cstdio>
 #include <iostream>
-#include <cstdlib>
-#include <vector>
-#include <list>
-#include <ctime>
-#include <iomanip>
 
-using namespace std;
-
-template <typename T>
-void apply(void(*f)(T&), T &x) 
+template <class FuncType, class FirstType, class ...ArgsType>
+void Apply( FuncType Func, FirstType &First, ArgsType&... Args )
 {
-	f(x);
+  Func(First);
+  Apply(Func, Args...);
+} /* End of 'Apply' function */
+
+static void Kek(int &X)
+{
+  X *= 10;
 }
 
-template <typename T, size_t S>
-void apply(void(*f)(T&), T a[]) 
+void main( void )
 {
-	for (size_t i(0); i < S; ++i)
-		apply<T>(f, a[i]);
-}
+  int x = 1, y = 2, z = 3, k = 4;
 
-template <typename T, typename U>
-void apply(void(*f)(T&), U &x) 
-{
-	for (auto &it : x)
-		apply<T>(f, it);
-}
+  Apply(Kek, x, y, z, k);
 
-template<typename T>
-void gen_rand(T &x) 
-{
-	x = rand() % 150 + rand() * 1.0 / rand();
-}
-
-template<typename T>
-void print(T &x) 
-{
-	cout << x << ' ';
-}
-
-vector <int> v(10);
-list <int> l(5);
-
-int main() {
-	srand(time(NULL));
-	cout << fixed << setprecision(1);
-	puts("Random generate");
-
-	puts("Single element:");
-	int x;
-	apply<int>(&gen_rand, x);
-	apply<int>(&print, x);
-
-	puts("\nArray double[10]:");
-	double a[10];
-	apply<double, 10>(&gen_rand, a);
-	apply<double, 10>(&print, a);
-
-	puts("\nList<int>(5):");
-	list <int> l(5);
-	apply<int, list<int> >(&gen_rand, l);
-	apply<int, list<int> >(&print, l);
-
-	puts("\nVector<double>(15):");
-	vector <double> v(15);
-	apply<double, vector<double> >(&gen_rand, v);
-	apply<double, vector<double> >(&print, v);
-	return 0;
+  std::cout << x << ' ' << y << ' ' << z << ' ' << k << std::endl;
+  system("pause");
 }
